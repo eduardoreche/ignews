d77daec1e3bb2eae10b55c2ed48543f18433038e
Eduardo Reche Lopes <eduardo.rechelopes@exxonmobil.com>
2021-07-21 21:40:50 -0300

    redirect user to full post when logged

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from 'next';
-import { getSession } from 'next-auth/client';
+import { getSession, useSession } from 'next-auth/client';
 import Head from 'next/head';
+import Link from 'next/link';
+import router, { useRouter } from 'next/router';
 import { RichText } from 'prismic-dom';
+import { useEffect } from 'react';
 import { getPrismicClient } from '../../../services/prismic';
 
 import styles from './../post.module.scss';
@@ -16,6 +19,13 @@ interface PostPreviewProps {
 }
 
 export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.activeSubscription) router.push(`/posts/${post.slug}`);
+  }, [session]);
+
   return (
     <>
       <Head>
@@ -30,6 +40,13 @@ export default function PostPreview({ post }: PostPreviewProps) {
             className={`${styles.postContent} ${styles.previewContent}`}
             dangerouslySetInnerHTML={{ __html: post.content }}
           />
+
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
         </article>
       </main>
     </>
